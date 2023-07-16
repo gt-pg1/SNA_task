2f0ad755bf959b592bd9f9afc932852f2ef8966a
gt-pg1 <shot66766@gmail.com>
2023-07-16 15:32:52 +0400

    Added cascading deletion of related records in the database model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,12 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, CheckConstraint
+from sqlalchemy import Column, \
+    Integer, \
+    String, \
+    Text, \
+    ForeignKey, \
+    CheckConstraint
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
 
 Base = declarative_base()
 
@@ -11,6 +18,7 @@ class User(Base):
     username = Column(String(50), unique=True, index=True, nullable=False)
     email = Column(String(120), unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+    posts = relationship('Post', backref='user', cascade='all,delete')
 
 
 class Post(Base):
@@ -19,13 +27,15 @@ class Post(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(100), nullable=False)
     content = Column(Text, nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("users.id", ondelete='CASCADE'))
+    likes = relationship('Like', backref='post', cascade='all,delete')
 
 
 class Like(Base):
     __tablename__ = "likes"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    post_id = Column(Integer, ForeignKey("posts.id"))
+    user_id = Column(Integer, ForeignKey("users.id", ondelete='CASCADE'))
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete='CASCADE'))
     value = Column(Integer, CheckConstraint('value=-1 OR value=1'), nullable=False)
+
